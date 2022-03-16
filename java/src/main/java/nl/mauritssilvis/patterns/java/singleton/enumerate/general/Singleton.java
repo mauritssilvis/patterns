ed20c99074b869d8def10e14e4b4310466a9fb23
Maurits Silvis <mauritssilvis@gmail.com>
2022-03-16 23:45:05 +0100

    Add an enum implementation of a singleton

---
+++ java/src/main/java/nl/mauritssilvis/patterns/java/singleton/enumerate/general/Singleton.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2022 Maurits H. Silvis
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package nl.mauritssilvis.patterns.java.singleton.enumerate.general;
+
+public enum Singleton {
+    SINGLETON
+}
