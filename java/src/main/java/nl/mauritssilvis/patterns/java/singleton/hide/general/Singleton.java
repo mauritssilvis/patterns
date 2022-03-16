e85bae62f63183bf4d3de93f588e72b5d8f4b303
Maurits Silvis <mauritssilvis@gmail.com>
2022-03-16 21:02:15 +0100

    Implement a singleton class with a private constructor

--- java/src/main/java/nl/mauritssilvis/patterns/java/singleton/hide/general/Singleton.java
+++ java/src/main/java/nl/mauritssilvis/patterns/java/singleton/hide/general/Singleton.java
@@ -6,10 +6,12 @@
 package nl.mauritssilvis.patterns.java.singleton.hide.general;
 
 public class Singleton {
+    private static final Singleton SINGLETON = new Singleton();
+
     private Singleton() {
     }
 
     public static Singleton getInstance() {
-        return new Singleton();
+        return SINGLETON;
     }
 }
