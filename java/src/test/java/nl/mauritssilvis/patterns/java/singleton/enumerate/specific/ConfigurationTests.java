e9a638553a8a01e36a1fca392b6f86fb4bdca33e
Maurits Silvis <mauritssilvis@gmail.com>
2022-03-16 23:52:44 +0100

    Add a specific enum singleton implementation

---
+++ java/src/test/java/nl/mauritssilvis/patterns/java/singleton/enumerate/specific/ConfigurationTests.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2022 Maurits H. Silvis
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+package nl.mauritssilvis.patterns.java.singleton.enumerate.specific;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ConfigurationTests {
+    @Test
+    void getTheSameConfigurationInstance() {
+        Configuration configuration1 = Configuration.CONFIGURATION;
+        Configuration configuration2 = Configuration.CONFIGURATION;
+
+        Assertions.assertSame(configuration1, configuration2, "Two Configuration enum instances were created.");
+    }
+
+    @Test
+    void ensureAccessibility() {
+        Configuration configuration = Configuration.CONFIGURATION;
+        configuration.setAccessible(true);
+
+        Assertions.assertTrue(configuration.isAccessible(), "The Configuration enum has an unexpected accessibility.");
+    }
+
+    @Test
+    void changeAccessibility() {
+        Configuration configuration1 = Configuration.CONFIGURATION;
+        configuration1.setAccessible(true);
+
+        Configuration configuration2 = Configuration.CONFIGURATION;
+        configuration2.setAccessible(false);
+
+        Assertions.assertAll(
+                () -> Assertions.assertFalse(configuration1.isAccessible(), "The first Configuration enum has an unexpected accessibility."),
+                () -> Assertions.assertFalse(configuration2.isAccessible(), "The second Configuration enum has an unexpected accessibility.")
+        );
+    }
+}
