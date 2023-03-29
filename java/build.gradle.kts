de8a573fa743ad08a24500812e6ac992d747d187
Maurits Silvis <mauritssilvis@gmail.com>
2023-03-29 23:27:58 +0200

    Start using the Gradle Kotlin DSL

---
+++ java/build.gradle.kts
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2022, 2023 Maurits H. Silvis
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+plugins {
+    java
+}
+
+group = "nl.mauritssilvis.patterns.java"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
---
+++ java/settings.gradle.kts
@@ -0,0 +1,7 @@
+/*
+ * Copyright © 2022, 2023 Maurits H. Silvis
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+rootProject.name = "java-patterns"
+
