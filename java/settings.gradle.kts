e354467b68eaf7a83c4136d8bfcf9201777ce097
Maurits Silvis <mauritssilvis@gmail.com>
2023-10-08 18:37:31 +0200

    Update the refreshVersions version

--- java/settings.gradle.kts
+++ java/settings.gradle.kts
@@ -5,7 +5,7 @@
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
-    id("de.fayard.refreshVersions") version "0.60.2"
+    id("de.fayard.refreshVersions") version "0.60.3"
 }
 
 rootProject.name = "java-patterns"
